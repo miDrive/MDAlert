55c15404de839178b965595128e172659f76452a
Chris Byatt <chris@midrive.com>
2018-06-05 13:45:40 +0100

    Have more spacing when we need it

--- Example/MDAlert/ViewController.swift
+++ Example/MDAlert/ViewController.swift
@@ -12,13 +12,37 @@ import MDAlert
 class ViewController: UIViewController {
 
     @IBAction func showAlert(_ sender: UIButton) {
-        let alert = MDAlertController(title: "Wow", message: "It's an alert!", showsCancel: true)
+        
+        // Alert with buttons
+//        let alert = MDAlertController(title: "Wow", message: "It's an alert!", showsCancel: true)
+//
+//        alert.addAction(MDAlertAction(title: "OK", style: .default, action: nil))
+//        alert.addAction(MDAlertAction(title: "OK", style: .destructive, action: nil))
+//        alert.addAction(MDAlertAction(title: "OK", style: .cancel, action: nil))
+//
+//        alert.show()
+        
+        // Alert no buttons
+//        let alert = MDAlertController(title: "Password reset", message: "Your request has been recieved, please check your email for instructions on resetting your password.")
+//
+//        alert.show()
+        
+        // Alert custom view no buttons
+        let view = UIView(frame: CGRect(x: 50, y: 50, width: 50, height: 130))
+        view.backgroundColor = .black
 
-        alert.addAction(MDAlertAction(title: "OK", style: .default, action: nil))
-        alert.addAction(MDAlertAction(title: "OK", style: .destructive, action: nil))
-        alert.addAction(MDAlertAction(title: "OK", style: .cancel, action: nil))
+        let alert = MDAlertController(title: "Wow", message: "This is an alert", customView: view, showsCancel: true)
 
         alert.show()
+        
+        // Alert custom view with Buttons
+//        let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 130))
+//        view.backgroundColor = .black
+//
+//        let alert = MDAlertController(title: "Wow", message: "This is an alert", customView: view, showsCancel: true)
+//        alert.addAction(MDAlertAction(title: "OK", style: .default, action: nil))
+//
+//        alert.show()
     }
 }
 
